5434369d5a266a851612f5ef49e58f3c8e2fc52d
bibek.regmi <vivekregmi83@gmail.com>
2024-04-02 11:05:58 +0545

    feat(Role): add role dto

---
+++ src/main/java/org/example/cpas/dto/RoleDto.java
@@ -0,0 +1,17 @@
+package org.example.cpas.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class RoleDto {
+
+    private int id;
+    private String name;
+    private String description;
+    private String code;
+    private String role_type;
+}
