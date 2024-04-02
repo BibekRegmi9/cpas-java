732909ef201846fddcf0949fd0d08a4189eba518
bibek.regmi <vivekregmi83@gmail.com>
2024-04-02 11:03:26 +0545

    feat(Role): add role entity

--- src/main/java/org/example/cpas/entities/Roles.java
+++ src/main/java/org/example/cpas/entities/Roles.java
@@ -1,5 +1,33 @@
 package org.example.cpas.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Getter
+@Setter
 public class Roles {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "role_type")
+    private String role_type;
 }
