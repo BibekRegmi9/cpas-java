b9b1c210a2376c7d4733dea85ccdc6c779ef6351
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-03 22:23:52 +0545

    feat(Update User): update user with roles complete

--- src/main/java/org/example/cpas/repository/UserRoleMappingRepository.java
+++ src/main/java/org/example/cpas/repository/UserRoleMappingRepository.java
@@ -2,6 +2,19 @@ package org.example.cpas.repository;
 
 import org.example.cpas.entities.UserRoleMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRoleMappingRepository extends JpaRepository<UserRoleMapping, Integer> {
+
+
+    @Query(value = "select  role_id as roleId from user_role_mapping urm where  urm.user_id=?1",nativeQuery = true)
+    public  Integer[] getAllRoleByUserId(Integer userId);
+
+    @Modifying
+    @Query(value = "delete from user_role_mapping where  user_id=?1 and role_id in (?2)",nativeQuery = true)
+    public void  deleteMapping(Integer userId,Integer[] roleId);
 }
