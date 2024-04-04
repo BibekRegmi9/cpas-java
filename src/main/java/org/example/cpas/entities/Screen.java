9d7dbf2eaf1250818379336b53a8df976c7d7c86
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-04 11:34:07 +0545

    feat(Screen): add Screen entity

---
+++ src/main/java/org/example/cpas/entities/Screen.java
@@ -0,0 +1,43 @@
+package org.example.cpas.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "screens")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Screen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "is_active")
+    private boolean is_active;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
