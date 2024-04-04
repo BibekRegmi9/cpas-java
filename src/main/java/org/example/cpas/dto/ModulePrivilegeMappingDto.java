f294ef8a9dc1a2ac296d5b2ba0faeca1bc0cc673
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-04 12:14:25 +0545

    feat(Module Privilege Mapping): add Module Previlege Mapping dto

---
+++ src/main/java/org/example/cpas/dto/ModulePrivilegeMappingDto.java
@@ -0,0 +1,21 @@
+package org.example.cpas.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ModulePrivilegeMappingDto {
+
+    private int id;
+    private String name;
+    private String description;
+    private String code;
+    private boolean is_active;
+
+    private int[] privilegeId;
+    private int[] moduleId;
+
+}
