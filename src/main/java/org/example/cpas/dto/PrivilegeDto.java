d60188f3cf828c737b3d3b0befcb2db9b1a196a6
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-04 09:40:01 +0545

    feat(Privilege): add privileges dto

---
+++ src/main/java/org/example/cpas/dto/PrivilegeDto.java
@@ -0,0 +1,20 @@
+package org.example.cpas.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PrivilegeDto {
+
+    private String name;
+
+    private String description;
+
+    private String code;
+
+    private boolean is_active;
+
+}
