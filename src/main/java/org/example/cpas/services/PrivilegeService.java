6382fe9544b187d2b4296ba678c240e28ed404e2
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-04 10:20:54 +0545

    feat(Privilege): add privilege service

--- src/main/java/org/example/cpas/services/PrivilegeService.java
+++ src/main/java/org/example/cpas/services/PrivilegeService.java
@@ -1,7 +1,20 @@
 package org.example.cpas.services;
 
 
+import org.example.cpas.dto.PrivilegeDto;
+
+import java.util.List;
+
 public interface PrivilegeService {
 
+    PrivilegeDto createPrivilege(PrivilegeDto privilege);
+
+    PrivilegeDto updatePrivilege(PrivilegeDto privilege, Integer privilegeId);
+
+    PrivilegeDto getPrivilege(Integer privilegeId);
+
+    List<PrivilegeDto> getAllPrivilege();
+
+    void deletePrivilege(Integer privilegeId);
 
 }
