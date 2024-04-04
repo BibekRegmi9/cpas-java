04aa813a2ccf160991371978c1b66442cbf6dd67
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-04 12:22:10 +0545

    feat(Module ): add Module repository interface

---
+++ src/main/java/org/example/cpas/repository/ModuleRepository.java
@@ -0,0 +1,6 @@
+package org.example.cpas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleRepository extends JpaRepository<Module, Integer> {
+}
