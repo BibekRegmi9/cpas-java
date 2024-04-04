1d9c29d1fff74e80645fb10649b34bf9f2b9b149
Bibek Regmi <vivekregmi83@gmail.com>
2024-04-04 16:00:44 +0545

    feat(Module): add module service ->  create module function

---
+++ src/main/java/org/example/cpas/services/impl/ModuleServiceImpl.java
@@ -0,0 +1,72 @@
+package org.example.cpas.services.impl;
+
+import org.example.cpas.dto.ModuleDto;
+import org.example.cpas.entities.Module;
+import org.example.cpas.entities.ModulePrivilegeMapping;
+import org.example.cpas.entities.Privilege;
+import org.example.cpas.repository.ModulePrivilegeMappingRepository;
+import org.example.cpas.repository.ModuleRepository;
+import org.example.cpas.repository.PrivilegeRepository;
+import org.example.cpas.services.ModuleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ModuleServiceImpl implements ModuleService {
+
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @Autowired
+    private ModulePrivilegeMappingRepository modulePrivilegeMappingRepository;
+
+    @Autowired
+    private PrivilegeRepository privilegeRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+
+    @Override
+    public ModuleDto createModule(ModuleDto moduleDto) {
+        Module module = this.modelMapper.map(moduleDto, Module.class);
+        Module savedModule = this.moduleRepository.save(module);
+
+        for(ModuleDto.PrivilegeDto mp: moduleDto.getPrivilege()){
+            if(mp != null){
+                Privilege privilege=this.privilegeRepository.findById(mp.getPrivilege_id()).orElse(null);
+                if(privilege!=null){
+                    ModulePrivilegeMapping createModulePrivilege = new ModulePrivilegeMapping();
+                    createModulePrivilege.setPrivilege(privilege);
+                    createModulePrivilege.setModule(savedModule);
+                    this.modulePrivilegeMappingRepository.save(createModulePrivilege);
+                }
+            }
+        }
+        return moduleDto;
+    }
+
+    @Override
+    public ModuleDto updateModule(ModuleDto module, Integer moduleId) {
+        return null;
+    }
+
+    @Override
+    public ModuleDto getModule(Integer moduleId) {
+        return null;
+    }
+
+    @Override
+    public List<ModuleDto> getAllModule() {
+        return null;
+    }
+
+    @Override
+    public void deleteModule(Integer moduleId) {
+
+    }
+}
